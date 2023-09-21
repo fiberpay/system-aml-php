5311a33399d609b4af6706db4fc177be9d231d43
Wojciech Czartoryjski <wczartoryjski@gmail.com>
2023-09-21 17:00:02 +0200

    example payloads update

--- examples/Example_payloads.php
+++ examples/Example_payloads.php
@@ -120,6 +120,102 @@ $ret = $client->createSoleProprietorshipParty(
     companyPhoneCountry: "48",
     companyPhoneNumber: "123123123"
 );
+<!-- CREATE COMPANY PARTY -->
+$ret = $client->createCompanyParty(
+    status: PartyStatus::ACTIVE,
+    companyName: "FiberPay",
+    taxIdNumber: "7010634566",
+    nationalBusinessRegistryNumber: "147302566",
+    tradeNames: ["FiberPay", "SystemAML"],
+    nationalCourtRegistryNumber: "0000512707",
+    businessActivityForm: "stock_company",
+    website: "fiberpay.pl",
+    references: "qwerty",
+    businessCountry: "PL",
+    businessCity: "Warszawa",
+    businessStreet: "Grzybowska",
+    businessHouseNumber: "4",
+    businessFlatNumber: "106",
+    businessPostalCode: "00-131",
+    companyEmailAdress: "info@fiberpay.pl",
+    companyPhoneCountry: "48",
+    companyPhoneNumber: "123123123",
+    servicesDescription: "Usługi płatnicze",
+    createdByName: "Wojtek",
+    mainPkdCode: "64.99.Z",
+    mainPkdName: "POZOSTAŁA FINANSOWA DZIAŁALNOŚĆ USŁUGOWA, GDZIE INDZIEJ NIESKLASYFIKOWANA, Z WYŁĄCZENIEM UBEZPIECZEŃ I FUNDUSZÓW EMERYTALNYCH",
+    pkdCodes: [
+        [
+        "pkdCode" => "58.29.Z",
+        "pkdName" => "DZIAŁALNOŚĆ WYDAWNICZA W ZAKRESIE POZOSTAŁEGO OPROGRAMOWANIA"
+        ],
+        [
+        "pkdCode" => "62.01.Z",
+        "pkdName" => "DZIAŁALNOŚĆ ZWIĄZANA Z OPROGRAMOWANIEM"
+        ]
+    ],
+    beneficiaries: [
+        [
+        "birthCountry" => "PL",
+        "directRights" => "ABB",
+        "birthCity" => "Warszawa",
+        "citizenship" => "PL",
+        "documentNumber" => "aze123123",
+        "documentType" => "id_card",
+        "firstName" => "Jan",
+        "lastName" => "Kowalski",
+        "ownedSharesAmount" => "45",
+        "ownedSharesUnit" => "%",
+        "personalIdentityNumber" => "64091098920",
+        "politicallyExposed" => "no",
+        "withoutExpirationDate" => false,
+        ],
+        [
+        "birthCountry" => "DE",
+        "directRights" => "3M",
+        "birthCity" => "Germany",
+        "birthDate" => "2002-10-01",
+        "citizenship" => "DE",
+        "documentNumber" => "aze423",
+        "documentType" => "id_card",
+        "firstName" => "Hans",
+        "lastName" => "Podolski",
+        "ownedSharesAmount" => "15",
+        "ownedSharesUnit" => "%",
+        "politicallyExposed" => "no",
+        "withoutExpirationDate" => false,
+        ],
+    ],
+    boardMembers: [
+        [
+        "birthCity" => "Warszawa",
+        "birthDate" => "2001-01-01",
+        "birthCountry" => "PL",
+        "citizenship" => "PL",
+        "description" => "Prezes",
+        "documentNumber" => "aze123123",
+        "documentType" => "id_card",
+        "firstName" => "Jan",
+        "lastName" => "Kowalski",
+        "personalIdentityNumber" => "31111161119",
+        "politicallyExposed" => "no",
+        "withoutExpirationDate" => false,
+        ],
+        [
+        "birthCity" => "Warszawa",
+        "birthDate" => "2001-01-01",
+        "birthCountry" => "PL",
+        "citizenship" => "PL",
+        "description" => "Wiceprezes",
+        "documentNumber" => "aze129923",
+        "documentType" => "id_card",
+        "firstName" => "Adam",
+        "lastName" => "Nowak",
+        "politicallyExposed" => "yes",
+        "withoutExpirationDate" => false,
+        ]
+    ]
+);
 <!-- GET PARTY -->
 $ret = $client->getParty("8s4a617n3xgf");
 <!-- CHANGE TRANSACTION STATUS -->
