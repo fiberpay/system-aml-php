b70af074ddee4adc612adc23d8a8c92c331ddae0
Wojciech Czartoryjski <wczartoryjski@gmail.com>
2023-09-22 12:48:27 +0200

    removed entity type sender

--- src/RequestParams/Transaction/EntityType.php
+++ src/RequestParams/Transaction/EntityType.php
@@ -5,7 +5,6 @@ namespace FiberPay\SystemAML\RequestParams\Transaction;
 enum EntityType: string
 {
     case RECEIVER = 'receiver';
-    case SENDER = 'sender';
     case SELLER = 'seller';
     case BUYER = 'buyer';
     case PAYER = 'payer';
